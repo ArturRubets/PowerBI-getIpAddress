81511da3772a2f02c08963ab3b24a6a0f32f8ae9
Рубец Артур Максимович <a.rubets@fb.lan>
2021-10-22 11:14:27 +0300

    create function get ip addres

--- src/visual.ts
+++ src/visual.ts
@@ -1,30 +1,4 @@
-/*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
 "use strict";
-
 import "core-js/stable";
 import "./../style/visual.less";
 import powerbi from "powerbi-visuals-api";
@@ -43,8 +17,7 @@ export class Visual implements IVisual {
     private settings: VisualSettings;
     private textNode: Text;
 
-    constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
+    constructor(options: VisualConstructorOptions) {       
         this.target = options.element;
         this.updateCount = 0;
         if (document) {
@@ -59,22 +32,26 @@ export class Visual implements IVisual {
     }
 
     public update(options: VisualUpdateOptions) {
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
+        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0])
+        
+        if (this.settings.general.push) {
+            this.getIpAddressFromApi()
+            .then(data => console.log(data.ip))
+            .catch(error => console.log(error))
         }
+    }
+
+    private getIpAddressFromApi(){
+        return fetch('https://api.ipify.org/?format=json')
+                .then(results => results.json())
+                .then(data => data)
+                .catch(error => error)
     }
 
     private static parseSettings(dataView: DataView): VisualSettings {
         return <VisualSettings>VisualSettings.parse(dataView);
     }
 
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
     public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
         return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
     }
